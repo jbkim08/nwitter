e25ce85586245cbd8267940a6a00ecdab9586edb
jbkim08 <jbkim08@gmail.com>
2023-11-06 19:57:47 +0900

    라우트와 CSS 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+import Login from './routes/Login';
+import CreateUser from './routes/CreateUser';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+  box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '',
+				element: <Home />,
+			},
+			{
+				path: '/profile',
+				element: <Profile />,
+			},
+		],
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
+	{
+		path: '/create-account',
+		element: <CreateUser />,
+	},
+]);
+
 function App() {
-	return <>정리화면</>;
+	return (
+		<>
+			<GlobalStyle />
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
